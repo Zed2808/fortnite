93d230eb22fce27e027d58ec476f9cf0b8406e51
Zachary Stevens <zed2808@gmail.com>
2018-03-18 15:52:18 -0400

    Also draw circles created by new points

--- plot.py
+++ plot.py
@@ -1,3 +1,5 @@
+#!/usr/local/bin/python3.6
+
 import numpy as np
 from matplotlib import pyplot as plt
 from fortnite import point_in_circle
@@ -9,14 +11,19 @@ x = [point[0] for point in points]
 y = [point[1] for point in points]
 
 circle = plt.Circle((0, 0), 2, color='r',  fill=False)
+new_center = plt.Circle((0, 0), 1, color='r', fill=False)
 
 ax = plt.gca()
 ax.cla()
 ax.set_xlim(-2.1, 2.1)
 ax.set_ylim(-2.1, 2.1)
 
+for i in range(N):
+    ax.add_artist(plt.Circle(points[i], 1, color='black', linewidth=0.1, fill=False, zorder=0))
+
 ax.set_aspect('equal')
-ax.scatter(x, y, s=1)
+ax.scatter(x, y, s=2, color='yellow')
 ax.add_artist(circle)
+ax.add_artist(new_center)
 plt.show()
 
